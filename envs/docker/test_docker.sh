4c073ddea18a8ea0493bbc4862ec712109d9a57b
kzkedzierska <31040515+kzkedzierska@users.noreply.github.com>
2024-08-21 01:29:30 +0200

    :truck: move files into a dedicated subdir

--- envs/docker/test_docker.sh
+++ envs/docker/test_docker.sh
@@ -2,7 +2,7 @@
 
 # This script is used to test the docker image built by the Dockerfile in the same directory.
 # The docker image is built by the following command:
-# docker build -t kzkedzierska/sc_foundation_evals .
+# docker build -t kzkedzierska/sc_foundation_evals[:tag] .
 
 # The script runs the docker image and executes the test.py script in the container.
 # The test.py script is a simple script that imports the sc_foundation_evals package and prints the version of the package.
